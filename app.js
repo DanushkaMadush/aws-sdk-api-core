0444c4b8c0f129359973a7d59b318064e314088f
danushkacodeolima <danushka@codeolima.com>
2025-02-06 09:39:41 +0530

    fix(app.js):Fixed commonJS ES module error

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import { fileURLToPath } from 'url';
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 import bucketsRouter from './routes/buckets.js';
 import objectsRouter from './routes/objects.js';
 
+const __filename = fileURLToPath(import.meta.url); // Get the current file path
+const __dirname = path.dirname(__filename); // Get the directory name
+
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); // ✅ Now this will work
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
@@ -22,6 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1/buckets', bucketsRouter);
-app.use('/api/v1/objects', objectsRouter); 
+app.use('/api/v1/objects', objectsRouter);
 
 export default app;
