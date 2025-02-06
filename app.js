1d4962cf4135ae51d1a2b76d0313d39cc3d81e66
danushkacodeolima <danushka@codeolima.com>
2025-02-06 20:35:37 +0530

    Added cors

--- app.js
+++ app.js
@@ -3,24 +3,26 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 import bucketsRouter from './routes/buckets.js';
 import objectsRouter from './routes/objects.js';
 
-const __filename = fileURLToPath(import.meta.url); // Get the current file path
-const __dirname = path.dirname(__filename); // Get the directory name
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename); 
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views')); // ✅ Now this will work
+app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
